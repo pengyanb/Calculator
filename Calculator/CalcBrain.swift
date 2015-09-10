c05d6533608eafab188fa8327595b2bf69053927
pengyanb <pengyanbing@msn.com>
2015-09-10 22:22:44 +1200

    IOS8_Swift_Calculator
    IOS8_Swift_Calculator

---
+++ Calculator/CalcBrain.swift
@@ -0,0 +1,185 @@
+//
+//  CalcBrain.swift
+//  Calculator
+//
+//  Created by Yanbing Peng on 4/09/15.
+//  Copyright (c) 2015 Yanbing Peng. All rights reserved.
+//
+
+import Foundation
+
+class CalcBrain
+{
+    private enum Op: Printable
+    {
+        case AppendOperand(Double)
+        case NullaryOperation(String, ()->Double?)
+        case UnaryOperation(String, (Double)->Double)
+        case BinaryOperation(String, (Double, Double)->Double)
+
+        var description:String
+        {
+            get{
+                switch self
+                {
+                case .AppendOperand(let operand):
+                    return "\(operand)"
+                case .NullaryOperation(let symbol, _):
+                    return symbol
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]();
+    private var knownOps = [String:Op]();
+    private var variableValues = Dictionary<String,Double>()
+    private var opPrecedence = Dictionary<String, Int>()
+
+    var errorStr = ""
+    
+    var description:String{
+        get{
+            return describ()
+        }
+    }
+    init()
+    {
+        knownOps["+"] = Op.BinaryOperation("+", {(op1:Double, op2:Double)->Double in return op2 + op1})
+        knownOps["−"] = Op.BinaryOperation("−", {(op1:Double, op2:Double)->Double in return op2 - op1})
+        knownOps["×"] = Op.BinaryOperation("×", {(op1:Double, op2:Double)->Double in return op2 * op1})
+        knownOps["÷"] = Op.BinaryOperation("÷", {(op1:Double, op2:Double)->Double in return op2 / op1})
+        knownOps["√"] = Op.UnaryOperation("√", {(op1:Double)->Double in return sqrt(op1)})
+        knownOps["sin"] = Op.UnaryOperation("sin", {(op1:Double)->Double in return sin(op1)})
+        knownOps["cos"] = Op.UnaryOperation("cos", {(op1:Double)->Double in return cos(op1)})
+        knownOps["π"] = Op.NullaryOperation("π", {()->Double? in return M_PI})
+        knownOps["M"] = Op.NullaryOperation("M", {()->Double? in return self.variableValues["M"]})
+        
+        opPrecedence["+"] = 1;
+        opPrecedence["-"] = 1;
+        opPrecedence["×"] = 2;
+        opPrecedence["÷"] = 2;
+    }
+    private func describ()->String
+    {
+        errorStr = ""
+        let describResult = describ(opStack, highestPrecedence: 0)
+        errorStr = describResult.errorString
+        return describResult.descriptionFragment+(describResult.showEqualSign ? " =" :"")
+    }
+    private func describ(opStack:[Op], highestPrecedence:Int)->(descriptionFragment:String, result:Double?, showEqualSign:Bool, errorString:String, remainingOpStack:[Op])
+    {
+        if !opStack.isEmpty
+        {
+            var remainingOpStack = opStack
+            let op = remainingOpStack.removeLast()
+            switch op{
+            case Op.AppendOperand(let digit):
+                let isInteger = floor(digit) == digit
+                return (isInteger ? "\(Int(digit))":"\(digit)", digit, false, "", remainingOpStack)
+            case Op.NullaryOperation(let symbol, let operation):
+                if let result = operation(){
+                    return (symbol, result, false, "", remainingOpStack)
+                }else{
+                    return (symbol, 1, false, "", remainingOpStack)
+                }
+            case Op.UnaryOperation(let symbol, let operation):
+                let describReturn = describ(remainingOpStack, highestPrecedence: 0)
+                if let result = describReturn.result{
+                    return (symbol+"("+describReturn.descriptionFragment+")", operation(result), true, "", describReturn.remainingOpStack)
+                }
+                else{
+                    return (symbol+"("+describReturn.descriptionFragment+")", nil, false, "Invalid Operation", describReturn.remainingOpStack)
+                }
+                
+            case Op.BinaryOperation(let symbol, let operation):
+                let currentPrecedence = opPrecedence[symbol]!
+                let needBracket = (currentPrecedence < highestPrecedence)
+                let describReturn1 = describ(remainingOpStack, highestPrecedence: currentPrecedence)
+                if let operand1 = describReturn1.result
+                {
+                    let describReturn2 = describ(describReturn1.remainingOpStack, highestPrecedence: currentPrecedence)
+
+                    if let operand2 = describReturn2.result
+                    {
+                        return ( (needBracket ? "(":"")+describReturn2.descriptionFragment+symbol+describReturn1.descriptionFragment+(needBracket ? ")":""), operation(operand1, operand2), true, "", describReturn2.remainingOpStack)
+                    }
+                    else
+                    {
+                        return ("(?"+symbol+describReturn1.descriptionFragment+")", nil, false, describReturn2.errorString, describReturn2.remainingOpStack)
+                    }
+                }
+                else
+                {
+                    return ("("+symbol+"?)", nil, false, describReturn1.errorString, describReturn1.remainingOpStack)
+                }
+            }
+        }
+        return (" ", nil, false, "No Operand", opStack);
+    }
+    private func evaluate(opStack:[Op])->(result:Double?, remainingOpStack:[Op])
+    {
+        if count(opStack) > 0{
+            var remainingOpStack = opStack
+            let op = remainingOpStack.removeLast()
+            switch op{
+            case Op.NullaryOperation(let symbol, let operation):
+                return (operation(), remainingOpStack)
+            case Op.AppendOperand(let digit):
+                return (digit, remainingOpStack)
+            case Op.UnaryOperation(let symbol, let operation):
+                let evaluatedReturn = evaluate(remainingOpStack)
+                if let op1 = evaluatedReturn.result{
+                    let result = operation(op1)
+                    return (result, evaluatedReturn.remainingOpStack)
+                }
+            case Op.BinaryOperation(let symbol, let operation):
+                let evaluatedReturn1 = evaluate(remainingOpStack)
+                if let op1 = evaluatedReturn1.result{
+                    let evaluatedReturn2 = evaluate(evaluatedReturn1.remainingOpStack)
+                    if let op2 = evaluatedReturn2.result
+                    {
+                        return (operation(op1, op2), evaluatedReturn2.remainingOpStack)
+                    }
+                }
+            }
+        }
+        return (nil, opStack)
+    }
+    func evaluate()->Double?
+    {
+        if !opStack.isEmpty
+        {
+            let evalutedResult = evaluate(opStack)
+            println("OpStacks \(opStack) evaluated to result \(evalutedResult.result) and remainingStack \(evalutedResult.remainingOpStack)")
+    
+            return evalutedResult.result
+        }
+        return nil
+    }
+    func clearStack()
+    {
+        opStack = [Op]();
+        variableValues = [String:Double]();
+    }
+    func setVariable(variableName:String, variableValue:Double)->Double?
+    {
+        variableValues[variableName] = variableValue
+        return evaluate()
+    }
+    func pushOperand(digit:Double)->Double?
+    {
+        opStack.append(Op.AppendOperand(digit))
+        return evaluate()
+    }
+    func performOperation(operation:String)->Double?
+    {
+        opStack.append(knownOps[operation]!)
+        return evaluate()
+    }
+    
+}
---
+++ Calculator/CalculatorViewController.swift
@@ -0,0 +1,132 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by Yanbing Peng on 4/09/15.
+//  Copyright (c) 2015 Yanbing Peng. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var stack: UILabel!
+    
+    @IBOutlet var buttonKeys: [UIButton]!{
+        didSet{
+          for button in buttonKeys
+          {
+            button.titleLabel?.adjustsFontSizeToFitWidth = true
+          }
+        }
+    }
+    
+    
+    let calcBrain  = CalcBrain()
+    var userIsInTheMiddleOfEnterring = false
+    var dotHasBeenEntered = false
+    var piEnteredPreviously = false
+    
+    var displayValue:Double?{
+        get{
+            if let numberValue = NSNumberFormatter().numberFromString(display.text!){
+                return numberValue.doubleValue
+            }
+            else
+            {
+                return nil
+            }
+        }
+        set{
+            stack.text = calcBrain.description
+            if let numberValue = newValue{
+                let isInteger = floor(numberValue) == numberValue
+                display.text = isInteger ? "\(Int(numberValue))":"\(numberValue)"
+                
+            }
+            else
+            {
+                display.text = calcBrain.errorStr
+            }
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        if !userIsInTheMiddleOfEnterring
+        {
+            display.text = sender.currentTitle!
+            userIsInTheMiddleOfEnterring = true
+        }
+        else
+        {
+            if (dotHasBeenEntered && sender.currentTitle == "."){
+                println("dot entered before, return")
+                return
+            }
+            else if sender.currentTitle == "."
+            {
+                println("dot entered")
+                dotHasBeenEntered = true
+            }
+            else
+            {
+                println("else triggered")
+            }
+            display.text = display.text! + sender.currentTitle!
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfEnterring = false
+        dotHasBeenEntered = false
+        piEnteredPreviously = false
+        if let displayValueUnwrap = displayValue{
+            if let result = calcBrain.pushOperand(displayValueUnwrap)
+            {
+                displayValue = result
+            }
+        }
+        
+    }
+    
+    @IBAction func clear() {
+        userIsInTheMiddleOfEnterring = false
+        dotHasBeenEntered = false
+        piEnteredPreviously = false
+        calcBrain.clearStack()
+        displayValue = 0;
+    }
+    
+    @IBAction func setVariableValue(sender: UIButton) {
+        if let value = NSNumberFormatter().numberFromString(display.text!)?.doubleValue
+        {
+            userIsInTheMiddleOfEnterring = false;
+            if let result = calcBrain.setVariable("M", variableValue: value)
+            {
+                displayValue = result
+            }
+        }
+    }
+    @IBAction func performOperation(sender: UIButton)
+    {
+        if piEnteredPreviously
+        {
+            piEnteredPreviously = false
+        }
+        else
+        {
+            if userIsInTheMiddleOfEnterring
+            {
+                enter()
+            }
+        }
+        
+        if sender.currentTitle == "π"
+        {
+            piEnteredPreviously = true
+        }
+        displayValue = calcBrain.performOperation(sender.currentTitle!)
+    }
+    
+}
